d75ec972d42d69ce7138951d46f80acd37d3e56e
Ghilas KASMI <73539312+bapn3454@users.noreply.github.com>
2022-05-11 09:25:46 +0200

    feature / get all transactions

--- blockchainApi/public/Web/js/scripts.js
+++ blockchainApi/public/Web/js/scripts.js
@@ -18,7 +18,12 @@ function setAllTransactions() {
         "http://localhost:3000/api/transactions",
         //{paramOne : 1, paramX : 'abc'},
         function(data) {
-           alert('page content: ' + data);
+          for (var i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            addRetrievedItem(data[i]);
+            //Do something
+        }
+           
         }
     );
 }
@@ -33,6 +38,53 @@ function search() {
     //        alert('page content: ' + data);
     //     }
     // );
+}
+
+function addRetrievedItem(transaction) {
+  let newItem = document.createElement('div');
+  newItem.className = "col mb-5";
+  let div1 = document.createElement('div');
+  div1.className = "card h-100";
+  let img = document.createElement('img');
+  img.src = "https://dummyimage.com/450x300/dee2e6/6c757d.jpg";
+  img.className = "card-img-top";
+
+  let div1_1 = document.createElement('div');
+  div1_1.className = "card-body p-4";
+  let div1_1_1 = document.createElement('div');
+  div1_1_1.className = "text-center";
+  let price = document.createElement('p');
+  // price.innerHTML = "25.99$";
+  price.innerHTML = transaction.price;
+  let h5 = document.createElement('h5');
+  // h5.textContent = "Item text";
+  h5.textContent = transaction.itemId;
+  h5.className = "fw-bolder";
+
+  div1_1_1.appendChild(h5);
+  div1_1_1.appendChild(price);
+  div1_1.appendChild(div1_1_1);
+
+  let div1_2 = document.createElement('div');
+  div1_2.className = "card-footer p-4 pt-0 border-top-0 bg-transparent";
+  let div1_2_1 = document.createElement('div');
+  div1_2_1.className = "text-center";
+  let buy = document.createElement("a");
+  buy.className = "btn btn-outline-dark mt-auto";
+  buy.href = "shopItem.html";
+  buy.innerHTML = "Buy";
+
+  div1_2_1.appendChild(buy);
+  div1_2.appendChild(div1_2_1);
+
+  
+  div1.appendChild(img);
+  div1.appendChild(div1_1);
+  div1.appendChild(div1_2);
+  newItem.appendChild(div1);
+
+  let itemContainer = document.getElementById("itemContainer").appendChild(newItem);
+  console.log(itemContainer);
 }
 
 function addItem() {
