bd2ada33cc4b3235f79730d95374d37f82a7a518
Ghilas KASMI <73539312+bapn3454@users.noreply.github.com>
2022-06-02 11:47:15 +0200

    register + loging added
    the current register login still need to be applied to other pages and an app state must be created

--- blockchainApi/public/Web/js/admin.js
+++ blockchainApi/public/Web/js/admin.js
@@ -24,3 +24,59 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
+function login() {
+    let email = document.getElementById("inputEmail").value;
+    let password = document.getElementById("inputPassword").value;
+    localStorage.setItem('email',email);
+    localStorage.setItem('password',password);
+    window.location.replace("index.html");
+    console.log(email + " "+ password);
+      // $.get(
+      //     "http://localhost:3000/api/transactions",
+      //     //{paramOne : 1, paramX : 'abc'},
+      //     function(data) {
+      //        alert('page content: ' + data);
+      //     }
+      // );
+  }
+
+  function register() {
+    let email = document.getElementById("inputEmail").value;
+    let password = document.getElementById("inputPassword").value;
+    let confirmPassword = document.getElementById("inputPasswordConfirm").value;
+    let firstName = document.getElementById("inputFirstName").value;
+    let lastName = document.getElementById("inputLastName").value;
+    // localStorage.setItem('email',email);
+    // localStorage.setItem('password',password);
+    // window.location.replace("index.html");
+    // console.log(email + " "+ password);
+    
+      if ( password === confirmPassword ) {
+          console.log(email+ " " + password + " "+ confirmPassword + " "+ firstName + " " + lastName);
+          let obj = {"email" : email, "pseudo" : email, "password" : password};
+          console.log(obj)
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:3000/api/transactions/", true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      myJSON = JSON.stringify(obj);
+      xhr.send(myJSON);
+
+
+      xhr.responseType = 'json';
+
+      xhr.onload = function () {
+          if (xhr.readyState === xhr.DONE) {
+              if (xhr.status === 200) {
+                localStorage.setItem('email',email);
+                localStorage.setItem('password',password);
+                window.location.href="index.html"
+              }
+          }
+      };
+      } else {
+          alert(" Les deux mots de passes saisies ne correspondent pas !");
+      }
+      
+  }
